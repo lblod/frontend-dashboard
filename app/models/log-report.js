5f8fa54d56d8e826bfdf4794c28016b2bac42f37
Oscar <lagartoverde97@gmail.com>
2020-01-08 13:46:36 +0100

    added period model and cleaned a little bit the ui

--- app/models/log-report.js
+++ app/models/log-report.js
@@ -5,5 +5,6 @@ import { belongsTo } from 'ember-data/relationships';
 export default DS.Model.extend({
   creationDate: attr('datetime'),
   author: attr(),
-  reportContent: belongsTo('report-content')
+  reportContent: belongsTo('report-content'),
+  period: belongsTo('period')
 });
--- app/routes/reports/report.js
+++ app/routes/reports/report.js
@@ -2,7 +2,8 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    return this.store.findRecord('log-report', params.id);
+    const model = this.store.findRecord('log-report', params.id);
+    return model
   },
   afterModel() { 
     this.transitionTo('reports.report.table');
