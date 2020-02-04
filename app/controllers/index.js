ffa52a6a1cc0b9a93ceaa5ad2e0cfab4fbce774f
Oscar <lagartoverde97@gmail.com>
2020-02-04 10:49:07 +0100

    fixed bug with pagination

---
+++ app/controllers/index.js
@@ -0,0 +1,6 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  page: 0,
+  size: 10,
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,15 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
+import { inject as service } from '@ember/service';
+
 export default Route.extend(AuthenticatedRouteMixin, DataTableRouteMixin, {
-  modelName: 'report'
+  modelName: 'report',
+  queryParams: {
+    page: {
+      refreshModel: true
+    }
+  },
+  session: service(),
+  store: service(),
 });
