8c36fe79ddafe30293b59ad1da456d09509c36a7
Oscar <lagartoverde97@gmail.com>
2020-02-19 11:18:46 +0100

    remove unused session code

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,30 +1,5 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
-import { warn } from '@ember/debug';
-import { inject as service } from '@ember/service';
 
 export default Route.extend(ApplicationRouteMixin, {
-  currentSession: service(),
-  moment: service(),
-
-  beforeModel() {
-    const moment = this.moment;
-    moment.setLocale('nl-be');
-    moment.setTimeZone('Europe/Brussels');
-    moment.set('defaultFormat', 'DD MMM YYYY, HH:mm');
-
-    return this._loadCurrentSession();
-  },
-  sessionAuthenticated() {
-    this._super(...arguments);
-    this._loadCurrentSession();
-  },
-  _loadCurrentSession() {
-    return this.currentSession.load().catch((e) => {
-      warn(e, { id: 'session-load-failure' });
-      this.session.invalidate();
-    });
-
-  }
-
 });
