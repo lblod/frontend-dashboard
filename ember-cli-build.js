5a49961fdcb224d374399d556dbdc677f28e06f4
Unknown <achrafatauil@hotmail.com>
2021-06-04 15:19:29 +0200

    install appuniversum + config & style files

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,15 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-cli-babel': {
+      includePolyfill: true
+    },
+    sassOptions: {
+      includePaths: [
+        'node_modules/@appuniversum/appuniversum',
+        'node_modules/@appuniversum/ember-appuniversum/app/styles',
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
