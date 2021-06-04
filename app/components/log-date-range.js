a867289e2da5c7708fb516ae37eafa55a1d1c335
Unknown <achrafatauil@hotmail.com>
2021-06-04 15:18:50 +0200

    upgrade log-date-range syntax

--- app/components/log-date-range.js
+++ app/components/log-date-range.js
@@ -1,54 +1,66 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import moment from 'moment';
 import { computed } from '@ember/object';
+import { action } from '@ember/object';
 
-export default Component.extend({
-  classNames: ['js-accordion', 'js-accordion--open'],
-  label: 'Periode verstuurd',
+export default class LogDateRangeComponent extends Component {
+  label = 'Periode verstuurd';
 
-  fromValue: null,  // ISO string
-  toValue: null,  // ISO string
+  get fromValue(){
+    return this.args.fromValue // ISO string
+  }
 
-  fromDate: computed('fromValue', function() {
+  get toValue(){
+    return this.args.toValue // ISO string
+  }
+
+  @computed('fromValue')
+  get fromDate() {
     try {
       return new Date(Date.parse(this.fromValue));
     } catch(e) {
       return null;
     }
-  }),
+  }
 
-  toDate: computed('toValue', function() {
+  @computed('toValue')
+  get toDate() {
     try {
       return new Date(Date.parse(this.toValue));
     } catch(e) {
       return null;
     }
-  }),
+  }
 
-  isFilterEnabled: computed('fromValue', 'toValue', function() {
+  @computed('fromValue', 'toValue')
+  get isFilterEnabled(){
     return this.fromValue || this.toValue;
-  }),
+  }
 
-  actions: {
+  @action
     resetFilter() {
-      this.onChangeFromValue(null);
-      this.onChangeToValue(null);
-    },
+      this.args.onChangeFromValue(null);
+      this.args.onChangeToValue(null);
+    }
 
+  @action
     initRangeFilter() {
       const yesterday = moment().subtract(1, 'day').startOf('day');
       const today = moment().endOf('day');
-      this.onChangeFromValue(yesterday.toDate().toISOString());
-      this.onChangeToValue(today.toDate().toISOString());
-    },
+      this.args.onChangeFromValue(yesterday.toDate().toISOString());
+      this.args.onChangeToValue(today.toDate().toISOString());
+    }
 
+  @action
     updateDate(varName, date) {
       const dateString = date.toISOString();
       if (varName == 'fromDate') {
-        this.onChangeFromValue(dateString);
+        this.args.onChangeFromValue(dateString);
       } else {
-        this.onChangeToValue(dateString);
+        this.args.onChangeToValue(dateString);
       }
     }
   }
-});
+
+
+
