a033dbb7e5997c5c6b429193482abd72a57c8eb4
Unknown <achrafatauil@hotmail.com>
2021-06-07 17:15:28 +0200

    refactor log-date-range component DDAU

--- app/components/log-date-range.js
+++ app/components/log-date-range.js
@@ -4,61 +4,34 @@ import { computed } from '@ember/object';
 import { action } from '@ember/object';
 
 export default class LogDateRangeComponent extends Component {
-  label = 'Periode verstuurd';
 
-  get fromValue(){
-    return this.args.fromValue // ISO string
-  }
-
-  get toValue(){
-    return this.args.toValue // ISO string
-  }
-
-  @computed('fromValue')
-  get fromDate() {
-    try {
-      return new Date(Date.parse(this.fromValue));
-    } catch(e) {
-      return null;
+  @computed('args.{fromValue,toValue}')
+    get isFilterEnabled(){
+      return this.args.fromValue || this.args.toValue;
     }
-  }
-
-  @computed('toValue')
-  get toDate() {
-    try {
-      return new Date(Date.parse(this.toValue));
-    } catch(e) {
-      return null;
-    }
-  }
-
-  @computed('fromValue', 'toValue')
-  get isFilterEnabled(){
-    return this.fromValue || this.toValue;
-  }
 
   @action
     resetFilter() {
-      this.args.onChangeFromValue(null);
-      this.args.onChangeToValue(null);
+      this.updateToValue(null);
+      this.updateFromValue(null);
     }
 
   @action
     initRangeFilter() {
       const yesterday = moment().subtract(1, 'day').startOf('day');
       const today = moment().endOf('day');
-      this.args.onChangeFromValue(yesterday.toDate().toISOString());
-      this.args.onChangeToValue(today.toDate().toISOString());
+      this.updateFromValue(yesterday.toDate());
+      this.updateToValue(today.toDate());
     }
 
   @action
-    updateDate(varName, date) {
-      const dateString = date.toISOString();
-      if (varName == 'fromDate') {
-        this.args.onChangeFromValue(dateString);
-      } else {
-        this.args.onChangeToValue(dateString);
-      }
+    updateToValue(value){
+      this.args.onChange('logDateTo', value && value.toISOString())
+    }
+
+  @action
+    updateFromValue(value){
+      this.args.onChange('logDateFrom', value && value.toISOString())
     }
   }
 
