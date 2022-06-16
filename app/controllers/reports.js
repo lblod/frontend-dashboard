dc0573dc12cf51a1e94c01b36af9d7cb799cf81d
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-06-16 03:49:58 +0200

    Renamed controller to correct name?

--- app/controllers/reports.js
+++ app/controllers/reports.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 
-export default class IndexController extends Controller {
+export default class ReportsController extends Controller {
   page = 0;
   size = 15;
   sort = '-created';
