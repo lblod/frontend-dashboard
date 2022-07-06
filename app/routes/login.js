8914b306c0e19a3d9229c979c5806f5eddb887c2
Elena Poelman <elena.poelman@redpencil.io>
2022-07-06 16:38:21 +0200

    add prohibitauth to login route

--- app/routes/login.js
+++ app/routes/login.js
@@ -5,6 +5,10 @@ export default class LoginRoute extends Route {
   @service session;
   @service router;
 
+  beforeModel() {
+    this.session.prohibitAuthentication('index');
+  }
+
   queryParams = {
     page: {
       refreshModel: true,
