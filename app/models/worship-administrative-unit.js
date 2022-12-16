8c5e0445cf427c932d92348db338c807b6c8c466
felix <ruiz.felixruiz@gmail.com>
2022-12-16 09:53:44 +0100

    added worship services

---
+++ app/models/worship-administrative-unit.js
@@ -0,0 +1,6 @@
+import { belongsTo, hasMany } from '@ember-data/model';
+import BestuurseenheidModel from './bestuurseenheid';
+
+export default class WorshipAdministrativeUnitModel extends BestuurseenheidModel {
+  
+}
