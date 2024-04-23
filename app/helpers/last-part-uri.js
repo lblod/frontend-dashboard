befa92a0e984a5e24eed69c3d4c4f414d7021eb5
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-04-23 17:33:10 +0200

    Don't use `array.lastObject`
    The prototype extensions are disabled so this no longer works.

--- app/helpers/last-part-uri.js
+++ app/helpers/last-part-uri.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function lastPartUri([uri]) {
-  const lastPart = uri.split('/').get('lastObject');
+  const lastPart = uri.split('/').at(-1);
   const formattedLastPart = lastPart.split('-').join(' ');
   return formattedLastPart.charAt(0).toUpperCase() + formattedLastPart.slice(1);
 }
