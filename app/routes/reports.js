771766fcfe1c17a2d80a9f79679d71c0720b96b5
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-03-25 18:02:45 +0100

    Remove our mixin usage

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -1,19 +1,50 @@
 import Route from '@ember/routing/route';
-import DataTableRouteMixin from 'ember-data-table/mixins/route';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-export default class IndexRoute extends Route.extend(DataTableRouteMixin) {
+export default class ReportsRoute extends Route {
   @service session;
   @service store;
 
-  beforeModel(transition) {
-    this.session.requireAuthentication(transition, 'index');
-  }
-
-  modelName = 'report';
   queryParams = {
     page: { refreshModel: true },
     size: { refreshModel: true },
     sort: { refreshModel: true },
   };
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'index');
+  }
+
+  model(params) {
+    const query = {
+      sort: params.sort,
+      page: {
+        number: params.page,
+        size: params.size,
+      },
+    };
+
+    // TODO: sending an empty filter param to backend returns []
+    if (params.filter) {
+      query['filter'] = params.filter;
+    }
+
+    return this.store.query('report', query);
+  }
+
+  @action
+  loading(transition) {
+    if (!transition.from || transition.from.name !== this.routeName) {
+      // We want the default loading screen if it's the initial page load or when we come from a different route
+      return true;
+    }
+
+    // eslint-disable-next-line ember/no-controller-access-in-routes
+    const controller = this.controller;
+    controller.isLoading = true;
+    transition.promise.finally(function () {
+      controller.isLoading = false;
+    });
+  }
 }
