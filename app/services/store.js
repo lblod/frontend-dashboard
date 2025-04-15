acea4494fdf8b30638f58e3630deb4d50ec08a7f
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-04-15 09:57:16 +0200

    Set up request manager using the incremental adoption guide
    https://github.com/emberjs/data/blob/468ef6fb55d7f8eb844af6094e9177ad59aad068/guides/cookbook/incremental-adoption-guide.md

--- app/services/store.js
+++ app/services/store.js
@@ -1,1 +1,6 @@
-export { default } from 'ember-data/store';
+import Store from 'ember-data/store';
+import { service } from '@ember/service';
+
+export default class MyStore extends Store {
+  @service requestManager;
+}
