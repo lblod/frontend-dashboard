0b52bbaa19e072c82f430f64c9c0a7227632d9ad
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-04-17 12:20:53 +0200

    Consistent datetime formatting
    Some dates were either not formatted or in a different way (without a specific reason), so this makes everything more consistent throughout the app.

--- app/helpers/format-datetime.js
+++ app/helpers/format-datetime.js
@@ -1,10 +1,10 @@
 import { format } from 'date-fns';
 
-export default function formatPlainDate(datetime, formatString) {
+export default function formatPlainDate(datetime) {
   if (!(datetime instanceof Date)) return '';
 
   try {
-    return format(datetime, formatString);
+    return format(datetime, 'dd/MM/yyyy HH:mm:ss');
   } catch (e) {
     console.error(e);
     return '';
